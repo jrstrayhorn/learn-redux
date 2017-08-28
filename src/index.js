7f6cf3c5a48fa626920f571a71bbf2b2a3c8b2f1
JR Strayhorn <jrstrayhorn@gmail.com>
2017-08-28 16:26:08 -0400

    working on adding redux

--- src/index.js
+++ src/index.js
@@ -1,43 +1,15 @@
 import React from 'react'
-import { render } from 'react-dom'
+import ReactDOM from 'react-dom'
 import App from './components/App'
+import storeFactory from './store'
 
-window.React = React
+const store = storeFactory()
 
-render(
-    <App />,
-    document.getElementById('react-container')
-)
+const render = () =>
+    ReactDOM.render(
+        <App store={store} />,
+        document.getElementById('react-container')
+    )
 
-/*import store from './store'
-import { removeColor, rateColor, addColor } from './actions'
-
-
-store.subscribe(() =>
-    console.log('color count:', store.getState().colors.length)
-)
-
-store.subscribe(() => {
-    localStorage['redux-store'] = JSON.stringify(store.getState())
-})
-
-//console.log( "Length of colors array before ADD_COLOR", store.getState().colors.length )
-//console.log( store.getState().sort )
-
-store.dispatch( addColor("Radical Red", "#432321") )
-
-store.dispatch( addColor("Mystical Blue", "#231234") )
-
-store.dispatch( rateColor("3315e1p5-3abl-0p523-30e4-8001l8yf3036", 4))
-/*
-store.dispatch({
-    type: "REMOVE_COLOR",
-    id: "3315e1p5-3abl-0p523-30e4-8001l8yf2412"
-})
-
-//console.log( "Length of colors array after ADD_COLOR", store.getState().colors.length )
-
-
-
-console.log('Go ahead, dispatch some actions...')
-*/
+store.subscribe(render)
+render()
