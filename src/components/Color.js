7cea1419e1e8952eb9d54de78bcfb61f0abe9b99
JR Strayhorn <jrstrayhorn@gmail.com>
2017-08-29 09:40:26 -0400

    Adding TimeAgo component, updating Color component

--- src/components/Color.js
+++ src/components/Color.js
@@ -1,17 +1,29 @@
-import { PropTypes } from 'react'
+import { PropTypes, Component } from 'react'
 import StarRating from './StarRating'
+import TimeAgo from './TimeAgo'
+import FaTrash from 'react-icons/lib/fa/trash-o'
 import '../../stylesheets/Color.scss'
 
-const Color = ({ title, color, rating=0, onRemove=f=>f, onRate=f=>f}) =>
-    <section className="color">
-        <h1>{title}</h1>
-        <button onClick={onRemove}>X</button>
-        <div className="color" style={{ backgroundColor: color }}>
-        </div>
-        <div>
-            <StarRating starsSelected={rating} onRate={onRate}></StarRating>
-        </div>
-    </section>
+class Color extends Component {
+    
+    render() {
+        const { title, color, rating, timestamp, onRemove, onRate} = this.props
+        return (
+            <section className="color" style={this.style}>
+                <h1 ref="title">{title}</h1>
+                <button onClick={onRemove}>
+                    <FaTrash />
+                </button>
+                <div className="color" style={{ backgroundColor: color }}>
+                </div>
+                <TimeAgo timestamp={timestamp} />
+                <div>
+                    <StarRating starsSelected={rating} onRate={onRate}></StarRating>
+                </div>
+            </section>
+        )
+    }
+}
 
 Color.propTypes = {
     title: PropTypes.string.isReqired,
@@ -19,6 +31,12 @@ Color.propTypes = {
     rating: PropTypes.number,
     onRemove: PropTypes.func,
     onRate: PropTypes.func
+}
+
+Color.defaultProps = {
+    rating: 0,
+    onRemove: f=>f,
+    onRate: f=>f
 }
 
 export default Color
