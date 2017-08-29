41e467df122059c48c0db737916d39bbc100c570
JR Strayhorn <jrstrayhorn@gmail.com>
2017-08-29 08:51:16 -0400

    Adding SortMenu component

--- src/components/App.js
+++ src/components/App.js
@@ -1,81 +1,14 @@
-import { Component } from 'react'
-import { v4 } from 'uuid'
+
 import AddColorForm from './AddColorForm'
 import ColorList from './ColorList'
+import SortMenu from './SortMenu'
 import '../../stylesheets/APP.scss'
 
-export default class App extends Component {
-    
-    constructor() {
-        super()
-        this.state = {
-            colors: [
-                {
-                    "id": "f9005b4e-975e-433d-a646-79df172e1dbb",
-                    "title": "Tomato",
-                    "color": "#d10012",
-                    "rating": 2
-                },
-                {
-                    "id": "58d9caee-6ea6-4d7b-9984-65b145031979",
-                    "title": "Lawn",
-                    "color": "#67bf4f",
-                    "rating": 1
-                },
-                {
-                    "id": "a5685c39-6bdc-4727-9188-6c9a00bf7f95",
-                    "title": "Party Pink",
-                    "color": "#ff00f7",
-                    "rating": 5
-                }
-            ]
-        }
-        this.addColor = this.addColor.bind(this)
-        this.rateColor = this.rateColor.bind(this)
-        this.removeColor = this.removeColor.bind(this)
+const App = ({ store }) =>
+    <div className="app">
+        <SortMenu store={store}></SortMenu>
+        <AddColorForm store={store}></AddColorForm>
+        <ColorList store={store}></ColorList>
+    </div>
 
-    }
-
-    addColor(title, color) {
-        const colors = [
-            ...this.state.colors,
-            {
-                id: v4(),
-                title,
-                color,
-                rating: 0
-            }
-        ]
-        this.setState({colors})
-    }
-
-    rateColor(id, rating) {
-        const colors = this.state.colors.map(color =>
-            (color.id !== id) ?
-                color : 
-                {
-                    ...color,
-                    rating
-                }
-        )
-        this.setState({colors})
-    }
-
-    removeColor(id) {
-        const colors = this.state.colors.filter(color => color.id !== id)
-        this.setState({colors})
-    }
-
-    render() {
-        const { addColor, rateColor, removeColor } = this
-        const {colors } = this.state
-        return (
-            <div className="app">
-                <AddColorForm onNewColor={addColor} />
-                <ColorList colors={colors}
-                        onRate={rateColor}
-                        onRemove={removeColor} />
-            </div>
-        )
-    }
-}
+export default App
