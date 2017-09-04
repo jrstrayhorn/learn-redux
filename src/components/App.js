d631b9ca56aa84ababe1dd9a3aafbb625eab6d81
stepforwardtech <jrstrayhorn@gmail.com>
2017-09-04 07:02:49 -0400

    Adding react-redux, refactoring to use react-redux provider to add store to context

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,11 @@
-import { PropTypes, Component } from 'react'
 import '../../stylesheets/APP.scss'
 import { Menu, NewColor, Colors } from './containers'
 
-class App extends Component {
+const App = () =>
+    <div clasName="app">
+        <Menu />
+        <NewColor />
+        <Colors />
+    </div>
 
-    getChildContext() {
-        return {
-            store: this.props.store
-        }
-    }
-
-    componentWillMount() {
-        this.unsubscribe = store.subscribe(
-            () => this.forceUpdate()
-        )
-    }
-
-    componentWillUnmount() {
-        this.unsubscribe()
-    }
-
-    render() {
-        return (
-            <div className="app">
-                <Menu />
-                <NewColor />
-                <Colors />
-            </div>
-        )
-    }
-}
-
-App.propTypes = {
-    store: PropTypes.object.isRequired
-}
-
-App.childContextTypes = {
-    store: PropTypes.object.isRequired
-}
-    
 export default App
